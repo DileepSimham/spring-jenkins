aec485b8626fac886c1c5ca4007f7e018e8864bf
Dileep Simham <dileep.simham483@gmail.com>
2023-05-31 19:16:23 +0530

    first commit

---
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -0,0 +1,28 @@
+package com.javatechie.jenkin.api;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	
+	static Logger logger=LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application started");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executedd...");
+		
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
